6fa86f2a1aa4330422045b7770e0f8fac01b07ca
Bob Paauwe <bpaauwe@yahoo.com>
2022-08-25 09:19:33 -0700

    version 1.0.7

--- vue.py
+++ vue.py
@@ -14,7 +14,7 @@ LOGGER = udi_interface.LOGGER
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([vue.Controller])
-        polyglot.start('1.0.6')
+        polyglot.start('1.0.7')
         control = vue.Controller(polyglot, "controller", "controller", "emporia VUE")
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
