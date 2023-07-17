008f535fad10d1113b7da0d96e14b097248baa61
Bob Paauwe <bpaauwe@yahoo.com>
2023-07-16 17:40:13 -0700

    Version 1.0.24

--- vue.py
+++ vue.py
@@ -187,7 +187,7 @@ def discover():
 if __name__ == "__main__":
     try:
         polyglot = udi_interface.Interface([])
-        polyglot.start('1.0.23')
+        polyglot.start('1.0.24')
 
         polyglot.subscribe(polyglot.CUSTOMPARAMS, parameterHandler)
         polyglot.subscribe(polyglot.POLL, poll)
